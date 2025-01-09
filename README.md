30cb7a0d0a2d38f0e1b09d87063e2d232d7e82fa
Jonas Lopes <jonasmassume@gmail.com>
2025-01-09 16:28:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto didático para aprender programação.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <img src="https://img.ifunny.co/images/78ac6a618fe2bb69bc434bfcbe357794b7002498ff1d033858bc9be3568e7161_1.webp"></img>
+function Home() {
+  return (
+    <img src="https://img.ifunny.co/images/78ac6a618fe2bb69bc434bfcbe357794b7002498ff1d033858bc9be3568e7161_1.webp"></img>
+  );
 }
 
 export default Home;
