a8d8625cad3928bedf7d3667c9d3f91641fb1e60
Jonas Lopes <jonasmassume@gmail.com>
2025-01-13 12:01:44 +0000

    alterando página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
 function Home() {
   return (
-    <img src="https://img.ifunny.co/images/78ac6a618fe2bb69bc434bfcbe357794b7002498ff1d033858bc9be3568e7161_1.webp"></img>
+    <div>
+      <h1>Você está preparado para o futuro?</h1>
+
+      <p>Se a sua resposta for sim, vocês está no lugar errado...</p>
+
+      <p>Esta é uma comunidade de pessoas inquietas e curiosas. </p>
+      <p>
+        Sabemos que não importa o quão atualizados estejam nossos conhecimentos.{" "}
+      </p>
+      <p>Tudo muda o tempo todo e precisamo continuar aprendendo, evoluindo.</p>
+
+      <a href="https://discord.com/invite/GqBqrjMN99?fbclid=PAZXh0bgNhZW0CMTEAAaa32ott8kDXieM3IdFa8I8zqCuOw4ukcmh4dv3nSxZNPMwIckOZ5DWOGqk_aem_qlZY04ejHBX80ct-Ng37Mg">
+        <img
+          width="50px"
+          src="https://uploads-ssl.webflow.com/6257adef93867e50d84d30e2/66b6ab21929f5be3edc1a08d_jobs_clyde.svg"
+        ></img>
+      </a>
+
+      <p> &copy; Evolvio LTDA </p>
+    </div>
   );
 }
 
