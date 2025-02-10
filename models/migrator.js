2db31e9a41463f7591620a8f4a47fe5b3a8d083f
jonaslvboass <162380873+jonaslvboass@users.noreply.github.com>
2025-02-10 10:44:36 -0300

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,39 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database";
+
+const defaultMigrationOptions = {
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  return runMigrations({ dryRun: true });
+}
+
+async function runPendingMigrations() {
+  return runMigrations({ dryRun: false });
+}
+
+async function runMigrations({ dryRun = true }) {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    return await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient: dbClient,
+      dryRun: dryRun,
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = { listPendingMigrations, runPendingMigrations };
+
+export default migrator;
